d2aa8daa2753ecd780ad7aeea6cd48033f7c3284
plandum <temik.0465@mail.ru>
2023-03-06 21:10:56 +0300

    mod authRouter

--- src/auth/authRouter.js
+++ src/auth/authRouter.js
@@ -2,7 +2,6 @@ const Router = require('express');
 const router = new Router();
 const controller = require('./authController');
 const { check } = require('express-validator');
-const roleMiddleware = require('../../middleware/roleMiddleware');
 
 router.post(
   '/registration',
